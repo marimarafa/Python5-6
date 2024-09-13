65e34c41e7660270a4deaeeb8910195789e74e5d
marimarafa <arafamarim166@gmail.com>
2024-09-13 16:36:36 +0200

    ..

--- server.py
+++ server.py
@@ -1,17 +1,25 @@
 from flask import Flask,render_template
 
 api = Flask(__name__)
+utenti = [['mario','rossi','M'],['gianni','derossi','M'],['Anita','Garibaldi','F']]
+
 
 @api.route('/', methods = ['GET'])
 def index():
     return render_template("index.html")
 
+
 @api.route('/regok', methods = ['GET'])
 def regok():
     return render_template("reg_ok.html")
 
+
 @api.route('/regko', methods = ['GET'])
 def regko():
+    return render_template("reg_ko.html")
+
+@api.route('/registrati', methods = ['GET'])
+def registrati():
     return render_template("reg_ko.html")
 
 api.run(host="0.0.0.0",port=8085)
