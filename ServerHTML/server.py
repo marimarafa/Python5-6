7bafa49efa3f585f5d5943205bb87b6f7e6ec046
marimarafa <arafamarim166@gmail.com>
2024-09-17 13:45:34 +0200

    ...

--- ServerHTML/server.py
+++ ServerHTML/server.py
@@ -3,11 +3,7 @@ from flask import Flask,render_template, request
 api = Flask(__name__)
 
 # Lista utenti con nome, password, genere e altro
-utenti = [['mario', 'password01', 'M'], ['gianni', 'password02', 'M'], ['Anita', 'password03', 'F']]
-
-@api.route('/loggati', methods = ['GET'])
-def loggati():
-    return render_template('index2.html')
+utenti = [['mario', 'password01', 'M', '0'], ['gianni', 'password02', 'M','0'], ['Anita', 'password03', 'F','0']]
 
 @api.route('/', methods=['GET'])
 def index():
@@ -27,14 +23,34 @@ def registra():
     nome = request.args.get("nome")
     password = request.args.get("password")
     genere = request.args.get("genere")
-    
-    # Controlla se l'utente esiste e i dati corrispondono
-    for utente in utenti:
-        if (utente[0].lower() == nome.lower()) and (utente[1] == password) and (utente[2] == genere):
-            return render_template('reg_ok.html')  # Accesso riuscito
+    utente = [nome, password,genere, '0']
 
+    # Controlla se l'utente esiste e i dati corrispondono
+    if utente in utenti:
+        if (utente[0].lower() == nome.lower()) and (utente[1] == password) and (utente[2] == genere):
+            if utente[3] == '0':
+                return render_template('reg_ok.html')  # Accesso riuscito
+            else:
+                return render_template('index2.html') #utente gia registrato
+            
     # Se non trova l'utente, reindirizza a "accesso negato"
     return render_template('reg_ko.html')
+
+@api.route('/loggati', methods=['GET'])
+def loggati():
+    nome = request.args.get("nome")
+    password = request.args.get("password")
+    genere = request.args.get("genere")
+    utente = [nome, password,genere, '1']
+
+    if utente in utenti:
+        if (utente[0].lower() == nome.lower()) and (utente[1] == password) and (utente[2] == genere):
+            if utente[3] == '1':
+                return f'Ciao {nome} . Sei {genere}' # Accesso riuscito
+            else:
+                return render_template('log_ko.html') #utente non registrato
+            
+    return render_template('index.html')
 
 if __name__ == '__main__':
     api.run(host="0.0.0.0", port=8085)
