8b7b40e32d51255e6aefe938a4ac3aa1e6b2106e
marimarafa <arafamarim166@gmail.com>
2024-09-18 22:46:54 +0200

    ...

--- ServerHTML/server2.py
+++ ServerHTML/server2.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 api = Flask(__name__)
 
 # Lista utenti con nome, password, genere e altro
-utenti = [['mario', 'password01', 'M', '0'], ['gianni', 'password02', 'M', '0'], ['Anita', 'password03', 'F', '0']]
+utenti = [['mario', 'password01', 'M', '1'], ['gianni', 'password02', 'M', '1'], ['Anita', 'password03', 'F', '1']]
 
 @api.route('/', methods=['GET'])
 def index():
@@ -23,34 +23,36 @@ def registra():
     nome = request.form.get("nome")
     password = request.form.get("password")
     genere = request.form.get("genere")
-    utente = [nome, password, genere, '0']
 
-    # Controlla se l'utente esiste e i dati corrispondono
+    # Controlla se l'utente è già registrato
     for u in utenti:
         if u[0].lower() == nome.lower() and u[1] == password and u[2] == genere:
-            if u[3] == '0':
-                u[3] = '1'  # Aggiorna lo stato dell'utente a registrato
-                return render_template('reg_ok.html')  # Accesso riuscito
-            elif u[3] == '1':
-                return render_template('index2.html')  # Utente già registrato
+            if u[3] == '1':  # Se l'utente è già registrato
+                return redirect(url_for('loggati'))  # Reindirizza alla pagina di login
 
-    return render_template('reg_ko.html')  # Accesso negato
+    # Se l'utente non esiste, aggiungilo alla lista utenti
+    nuovo_utente = [nome, password, genere, '1']
+    utenti.append(nuovo_utente)
 
-@api.route('/loggati', methods=['POST'])
+    return render_template('reg_ok.html')  # Mostra pagina di registrazione riuscita
+
+@api.route('/loggati', methods=['GET', 'POST'])
 def loggati():
-    nome = request.form.get("nome")
-    password = request.form.get("password")
-    genere = request.form.get("genere")
+    if request.method == 'POST':
+        nome = request.form.get("nome")
+        password = request.form.get("password")
+        genere = request.form.get("genere")
 
-    # Cerca l'utente nella lista
-    for u in utenti:
-        if u[0].lower() == nome.lower() and u[1] == password and u[2] == genere:
-            if u[3] == '1':
-                return f'Ciao {nome}. Sei {genere}.' + render_template('log_ok.html')  # Accesso riuscito
-            else:
-                return render_template('log_ko.html')  # Utente non registrato
+        # Cerca l'utente nella lista
+        for u in utenti:
+            if u[0].lower() == nome.lower() and u[1] == password and u[2] == genere:
+                if u[3] == '1':
+                    return f'Ciao {nome}. Sei {genere}.' + render_template('log_ok.html')  # Accesso riuscito
+                else:
+                    return render_template('log_ko.html')  # Utente non registrato
 
-    return render_template('index.html')  # Accesso negato
+        return render_template('log_ko.html')  # Credenziali errate
+    return render_template('login.html')  # Mostra il form di login
 
 @api.route('/logok', methods=['GET'])
 def logok():
