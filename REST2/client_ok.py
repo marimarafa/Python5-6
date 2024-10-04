503d49e54c886418affd2a90de0ce85f6e1f63be
marimarafa <arafamarim166@gmail.com>
2024-10-04 15:24:33 +0200

    ..

--- REST2/client_ok.py
+++ REST2/client_ok.py
@@ -24,6 +24,23 @@ def GetCodicefiscale():
     cod = input('Inserisci codice fiscale: ')
     return {"codFiscale": cod}
 
+def EseguiOperazione(iOper,sServizio , dDatiToSend):
+    try:
+        if iOper == 1: #requests.post per aggiungere qualcosa 
+            response = requests.post(sServizio,json= dDatiToSend)
+        if iOper == 2: #requests.get per stampare qualcosa 
+            response = requests.get(sServizio)
+        if iOper == 3: #requests.put per le modifiche 
+            response = requests.put(sServizio,json= dDatiToSend)
+        if iOper == 4: 
+            response = requests.delete(sServizio,json= dDatiToSend)
+        if response.status_code == 200:
+            print(response.json())
+        else:
+            print("Attenzione , errore " + str(response.status_code))
+    except:
+        print("Problemi di comunicazione con il server , riprova piu tardi")
+
 while True:
     print("\nOperazioni disponibili:")
     print("1. Inserisci cittadino")
@@ -34,43 +51,41 @@ while True:
 
 
     try:
-        sOper = int(input("Cosa vuoi fare? "))
+        iOper = int(input("Cosa vuoi fare? "))
     except ValueError:
         print("Inserisci un numero valido!")
         continue
 
 
-    if sOper == 1:
+    if iOper == 1:
         print("Aggiunta cittadino")
         api_url = base_url + "/add_cittadino"
         jsonDataRequest = GetDatiCittadino()
-        response = requests.post(api_url, json=jsonDataRequest)
+        EseguiOperazione(1,api_url,jsonDataRequest)
 
     # Richiesta dati cittadino
-    elif sOper == 2:
+    elif iOper == 2:
         print("Richiesta dati cittadino")
         api_url = base_url + "/read_cittadino"
         jsonDataRequest = GetCodicefiscale()
-        response = requests.get(api_url + "/" + jsonDataRequest['codFiscale'])
-        print(response.json())
+        EseguiOperazione(2,api_url + "/" + jsonDataRequest['codFiscale'])
+        
 
-    elif sOper == 3:
+    elif iOper == 3:
         print("Modifica cittadino")
         api_url = base_url + "/update_cittadino"
         jsonDataRequest = GetDatiCittadino()
-        response = requests.post(api_url, json=jsonDataRequest)
-        print(response.json())
+        EseguiOperazione(3,api_url,jsonDataRequest)
 
 
-    elif sOper == 4:
+    elif iOper == 4:
         print("Eliminazione cittadino")
         api_url = base_url + "/elimina_cittadino"
         jsonDataRequest = GetCodicefiscale()
-        response = requests.post(api_url, json=jsonDataRequest)
-        print(response.json())
+        EseguiOperazione(4,api_url,jsonDataRequest)
 
 
-    elif sOper == 5:
+    elif iOper == 5:
         print("Buona giornata!")
         sys.exit()
 
