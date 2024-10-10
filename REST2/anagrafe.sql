6061b0edb76d8575fbe9369aa6072e07a8438fd4
marimarafa <arafamarim166@gmail.com>
2024-10-10 12:11:16 +0200

    ..

--- REST2/anagrafe.sql
+++ REST2/anagrafe.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE Anagrafe(
+CREATE TABLE Cittadini(
     codice_fiscale VARCHAR(16) PRIMARY KEY,
     nome VARCHAR(20),
     cognome VARCHAR(20),
@@ -13,17 +13,14 @@ CREATE TABLE Utenti (
     note TEXT;
 )
 
-INSERT INTO Anagrafe (codice_fiscale, nome, cognome, data_nascita)
+INSERT INTO Cittadini (codice_fiscale, nome, cognome, data_nascita)
 VALUES 
 ('dctnst34b56h501u', 'Mario', 'de martino', '1980-05-21'),
 ('dctnst34b56h501a', 'Gianni', 'de martino', '1980-05-21'),
 ('dctnst34b56h502w', 'Silvia', 'de martino', '1980-05-21'),
-('aaaaa', 'Paolo', 'Rossi', '1990-02-24'),
-('asddsa', 'adsadads', 'asdadsads', NULL), 
-('dsds', 'Roberto', 'Malatesta', '2000-01-10'),
-('VLDPTN02H04Z145J', 'Vladimir', 'Putin', '1998-04-14'),
-('GVNBNN01L04R132E', 'Gio', 'Ban', '2001-02-21'),
-('CHTGPT01G01K000J', 'Chat', 'GPT', '2001-01-01');
+('aaaaa', 'Paolo', 'Rossi', 'dctnst34b56h502w','1990-02-24')
+
+
 
 
 INSERT INTO Utenti (username , password, privilegi, note)
