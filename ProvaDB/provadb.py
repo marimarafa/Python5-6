6c5ad17c2994a8ef55d12bff9464dbaa18f5e4d6
marimarafa <arafamarim166@gmail.com>
2024-10-10 13:06:04 +0200

    ..

--- ProvaDB/provadb.py
+++ ProvaDB/provadb.py
@@ -13,7 +13,7 @@ if cur is None:
 	print("Errore connessione al DB")
 	sys.exit()
 
-sQuery = "select * from ordini limit 5;"
+sQuery = "select * from cittadini limit 5;"
 iNumRows = db.read_in_db(cur,sQuery)
 for ii in range(0,iNumRows):
 	myrow = db.read_next_row(cur)
