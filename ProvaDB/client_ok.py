e461cf31a374cfa144b1054eaeeb0c7309b8b935
marimarafa <arafamarim166@gmail.com>
2024-10-17 17:55:34 +0200

    ..

---
+++ ProvaDB/client_ok.py
@@ -0,0 +1,128 @@
+
+
+import requests, json, sys
+
+
+base_url = "https://127.0.0.1:8080"
+
+
+def GetDatiCittadino():
+    nome = input("Inserisci il nome: ")
+    cognome = input("Inserisci il cognome: ")
+    dataN = input("Inserisci la data di nascita (gg/mm/aaaa): ")
+    codF = input("Inserisci il codice fiscale: ")
+    datiCittadino = {
+        "nome": nome, 
+        "cognome": cognome, 
+        "dataNascita": dataN, 
+        "codFiscale": codF
+    }
+    return datiCittadino
+
+
+def GetCodicefiscale():
+    cod = input('Inserisci codice fiscale: ')
+    return {"codFiscale": cod}
+
+def EseguiOperazione(iOper,sServizio , dDatiToSend):
+    try:
+        if iOper == 1: #requests.post per aggiungere qualcosa 
+            response = requests.post(sServizio,json= dDatiToSend,verify=False)
+        if iOper == 2: #requests.get per stampare qualcosa 
+            response = requests.get(sServizio,verify = False)
+        if iOper == 3: #requests.put per le modifiche 
+            response = requests.put(sServizio,json= dDatiToSend,verify=False)
+        if iOper == 4: 
+            response = requests.delete(sServizio,json= dDatiToSend,verify = False)
+        if response.status_code == 200:
+            print(response.json())
+        else:
+            print("Attenzione , errore " + str(response.status_code))
+    except:
+        print("Problemi di comunicazione con il server , riprova piu tardi")
+
+def EffetuaprimoLogin():
+    global sUsername,sPassword,Privilegio,iPrimoLoginEffetuato
+    sUsername = input("Inserisci username: ")
+    sPassword = input("Inserisci la password: ")
+    dUser = {
+        "username " : sUsername ,
+        "password" : sPassword 
+    }
+    try:
+        api_url = base_url + "/login"
+        response = requests.post(api_url,json= dUser,verify=False)
+
+        if response.status_code == 200:
+            jsonResponse = response.json()
+            if jsonResponse["Esito"] == "000":
+                Privilegio = dUser["Privilegio"]
+                iPrimoLoginEffetuato = 1
+    except:
+        print("Attenzione , problemi di comunicazione con il server, Riprova piu tardi.")
+        iPrimoLoginEffetuato = 0
+
+print("Benvenuti al Comune - sede locale")
+iPrimoLoginEffetuato = 0
+sUsername = ""
+sPassword = ""
+Privilegio = ""
+while iPrimoLoginEffetuato == 0:
+        EffetuaprimoLogin()
+    
+   
+
+flag = 0
+while flag == 0:
+    print("\nOperazioni disponibili:")
+    print("1. Inserisci cittadino")
+    print("2. Richiedi cittadino")
+    print("3. Modifica cittadino")
+    print("4. Elimina cittadino")
+    print("5. Esci")
+
+
+    try:
+        iOper = int(input("Cosa vuoi fare? "))
+    except ValueError:
+        print("Inserisci un numero valido!")
+        continue
+
+
+    if iOper == 1:
+        print("Aggiunta cittadino")
+        api_url = base_url + "/add_cittadino"
+        jsonDataRequest = GetDatiCittadino()
+        EseguiOperazione(1,api_url,jsonDataRequest)
+
+    # Richiesta dati cittadino
+    elif iOper == 2:
+        print("Richiesta dati cittadino")
+        api_url = base_url + "/read_cittadino"
+        jsonDataRequest = GetCodicefiscale()
+        EseguiOperazione(2,api_url + "/" + jsonDataRequest['codFiscale'],None)
+        
+
+    elif iOper == 3:
+        print("Modifica cittadino")
+        api_url = base_url + "/update_cittadino"
+        jsonDataRequest = GetDatiCittadino()
+        EseguiOperazione(3,api_url,jsonDataRequest)
+
+
+    elif iOper == 4:
+        print("Eliminazione cittadino")
+        api_url = base_url + "/elimina_cittadino"
+        jsonDataRequest = GetCodicefiscale()
+        EseguiOperazione(4,api_url,jsonDataRequest)
+
+
+    elif iOper == 5:
+        print("Buona giornata!")
+        sys.exit()
+
+    else:
+        print("Operazione non disponibile, riprova.")
+
+
+#
---
+++ ProvaDB/server_ok.py
@@ -0,0 +1,108 @@
+from flask import Flask, jsonify, request
+import dbclient as db
+
+api = Flask(__name__)
+
+
+file_path = "anagrafe.json"
+loginFile_path = "utenti.json"
+
+cur = db.connect()
+
+@api.route("/login" , methods = ["POST"])
+def GestisciLogin():
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        jsonReq = request.json
+
+# utenti [1chiave][2chiave]-> stampa il valore del valore (il primo valore è un dizionario)
+
+        username = jsonReq["username"]
+        password = cur[username]["password"]
+        privilegi = utenti[username]["privilegi"]
+
+        if username in utenti and password == jsonReq["password"]:
+            return jsonify({"Esito": "000", "Msg": "Login effetuato con successo", "Privilegio":privilegi}), 200
+        else:
+            return jsonify({"Esito": "001", "Msg": "Utente non trovato"}) , 200
+    else:
+        return jsonify({"Esito": "002", "Msg": "Formato richiesta non valido"}), 200
+
+# def Ha_privilegi_scrittura():
+#     jsonReq = request.json
+#     username = jsonReq.get('username')
+#     #login = GestisciLogin()
+    
+#     if username in utenti:
+#         privilegi = utenti[username]["privilegi"]
+#         password = utenti[username]["password"]
+#         if privilegi == "w":  # Controllo dei privilegi
+#             return True, utenti[username],utenti[password]
+#         return False ,'Accesso negato. Privilegi insufficienti per eseguire questa operazione.'
+#     return None,'utente non trovato.'
+
+cittadini = JsonDeserialize(file_path)
+@api.route('/add_cittadino', methods=['POST'])
+def GestisciAddCittadino():
+    # autorizzato ,utente = Ha_privilegi_scrittura()
+    # while autorizzato == True :
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        jsonReq = request.json
+        codice_fiscale = jsonReq.get('codFiscale')
+        if codice_fiscale in cittadini:
+            return jsonify({"Esito": "001", "Msg": "Cittadino già esistente"}), 200
+        else:
+            cittadini[codice_fiscale] = jsonReq
+            JsonSerialize(cittadini, file_path) 
+            return jsonify({"Esito": "000", "Msg": "Cittadino aggiunto con successo"}), 200
+    else:
+        return jsonify({"Esito": "002", "Msg": "Formato richiesta non valido"}), 200
+
+# < argomento > è lo stesso argomento passato nella funzione
+@api.route('/read_cittadino/<codice_fiscale>', methods=['GET'])
+def read_cittadino(codice_fiscale): # parametro passsato nella url
+    # autorizzato ,utente = Ha_privilegi_scrittura()
+    # while autorizzato == True  or autorizzato == False:
+    cittadino = cittadini.get(codice_fiscale)
+    if cittadino:
+        return jsonify({"Esito": "000", "Msg": "Cittadino trovato", "Dati": cittadino}), 200
+    else:
+        return jsonify({"Esito": "001", "Msg": "Cittadino non trovato"}), 200
+
+@api.route('/update_cittadino', methods=['PUT'])
+def update_cittadino():
+    # autorizzato ,utente = Ha_privilegi_scrittura()
+    # while autorizzato == True :
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        jsonReq = request.json
+        codice_fiscale = jsonReq.get('codFiscale')
+        if codice_fiscale in cittadini:
+            cittadini[codice_fiscale] = jsonReq
+            JsonSerialize(cittadini, file_path)  
+            return jsonify({"Esito": "000", "Msg": "Cittadino aggiornato con successo"}), 200
+        else:
+            return jsonify({"Esito": "001", "Msg": "Cittadino non trovato"}), 200
+    else:
+        return jsonify({"Esito": "002", "Msg": "Formato richiesta non valido"}), 200
+
+
+@api.route('/elimina_cittadino', methods=['DELETE'])
+def elimina_cittadino():
+    # autorizzato ,utente = Ha_privilegi_scrittura()
+    # while autorizzato == True :
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        cod = request.json.get('codFiscale')
+        if cod in cittadini:
+            del cittadini[cod]
+            JsonSerialize(cittadini, file_path)  
+            return jsonify({"Esito": "000", "Msg": "Cittadino rimosso con successo"}), 200
+        else:
+            return jsonify({"Esito": "001", "Msg": "Cittadino non trovato"}), 200
+    else:
+        return jsonify({"Esito": "002", "Msg": "Formato richiesta non valido"}), 200
+
+api.run(host="127.0.0.1", port=8080, ssl_context = "adhoc")
+
