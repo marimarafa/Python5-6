65f13ef2a8f66f440e6fefdf63f1d8e265e835cd
marimarafa <arafamarim166@gmail.com>
2024-12-05 13:15:14 +0100

    ..

--- REST4/server_db.py
+++ REST4/server_db.py
@@ -118,5 +118,5 @@ def elimina_cittadino():
     else:
         return jsonify({"Esito": "002", "Msg": "Formato richiesta non valido"}), 200
 
-api.run(host="127.0.0.1", port=8080, ssl_context = "adhoc")
+api.run(host="127.0.0.1", port=8080)
 
